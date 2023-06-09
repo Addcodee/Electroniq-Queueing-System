bc4154fd8983bb711f46e456bcb459fd49d42746
Alibek <musicchannel0p@gmail.com>
2023-06-09 16:01:42 +0600

    webpack

---
+++ src/components/Products.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./products.scss";
+
+const Products = () => {
+  return (
+    <div>
+      <div className="root">
+        <span>dsfms;dkfm</span>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
